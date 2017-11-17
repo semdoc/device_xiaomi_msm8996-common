LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Allow control over PocketMode service
 * Once the service is running, proximity sensor is constantly active
   when the display is turned off, resulting into a residual increase
   in battery consumption. Add a toggle so that users can decide whether
   they accept that and prefer to prevent accidental wake-ups triggered
   by the fingerprint sensor.
 * Only keep main service running if fingerprint wake-up feature is
   enabled at the same time as accidental wake-up prevention feature.
 * Set PocketMode as a required module of ConfigPanel, to make sure
   it is only shipped on devices building the latter.

Change-Id: I5941f445d0bd77be1efa9ffcde2d9854e6446c3d

configpanel/src/com/cyanogenmod/settings/device/ButtonSettingsFragment.java
@@ -17,12 +17,14 @@
 
 package com.cyanogenmod.settings.device;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
@@ -47,10 +49,16 @@ public class ButtonSettingsFragment extends PreferenceFragment
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
         String node = Constants.sBooleanNodePreferenceMap.get(preference.getKey());
         if (!TextUtils.isEmpty(node) && FileUtils.isFileWritable(node)) {
             boolean value = (Boolean) newValue;
             FileUtils.writeLine(node, value ? "1" : "0");
+            if (Constants.FP_WAKEUP_KEY.equals(preference.getKey())) {
+                value &= prefs.getBoolean(Constants.FP_POCKETMODE_KEY, false);
+                Constants.broadcastCustIntent(getContext(), value);
+            }
             return true;
         }
         node = Constants.sStringNodePreferenceMap.get(preference.getKey());
@@ -58,6 +66,12 @@ public class ButtonSettingsFragment extends PreferenceFragment
             FileUtils.writeLine(node, (String) newValue);
             return true;
         }
+
+        if (Constants.FP_POCKETMODE_KEY.equals(preference.getKey())) {
+            Constants.broadcastCustIntent(getContext(), (Boolean) newValue);
+            return true;
+        }
+
         return false;
     }
 
@@ -88,6 +102,10 @@ public class ButtonSettingsFragment extends PreferenceFragment
                 l.setEnabled(false);
             }
         }
+
+        // Initialize other preferences whose keys are not associated with nodes
+        SwitchPreference b = (SwitchPreference) findPreference(Constants.FP_POCKETMODE_KEY);
+        b.setOnPreferenceChangeListener(this);
     }
 
     private void updatePreferencesBasedOnDependencies() {

configpanel/src/com/cyanogenmod/settings/device/Startup.java
@@ -22,6 +22,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 
 import com.cyanogenmod.settings.device.utils.Constants;
@@ -36,6 +38,7 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent intent) {
         final String action = intent.getAction();
         if (cyanogenmod.content.Intent.ACTION_INITIALIZE_CM_HARDWARE.equals(action)) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
             // Disable button settings if needed
             if (!hasButtonProcs()) {
@@ -58,6 +61,12 @@ public class Startup extends BroadcastReceiver {
                             " failed while restoring saved preference values");
                     }
                 }
+
+                // Send initial broadcasts
+                final boolean shouldEnablePocketMode =
+                        prefs.getBoolean(Constants.FP_WAKEUP_KEY, false) &&
+                        prefs.getBoolean(Constants.FP_POCKETMODE_KEY, false);
+                Constants.broadcastCustIntent(context, shouldEnablePocketMode);
             }
         }
     }

pocketmode/src/com/cyanogenmod/pocketmode/Startup.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,15 +20,20 @@ package com.cyanogenmod.pocketmode;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserHandle;
 import android.util.Log;
 
-public class BootCompletedReceiver extends BroadcastReceiver {
+public class Startup extends BroadcastReceiver {
 
     private static final String TAG = "XiaomiPocketMode";
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
-        Log.d(TAG, "Starting");
-        context.startService(new Intent(context, PocketModeService.class));
+    public void onReceive(final Context context, final Intent intent) {
+        final String action = intent.getAction();
+        if (cyanogenmod.content.Intent.ACTION_INITIALIZE_CM_HARDWARE.equals(action)) {
+            Log.d(TAG, "Starting up receiver service");
+            context.startServiceAsUser(new Intent(context, PocketModeReceiverService.class),
+                    UserHandle.CURRENT);
+        }
     }
 }

semdoc/device_xiaomi_msm8996-common

 * Keep the receiver that listens for the screen status registered only
   if the fingerprint wake-up feature is enabled at the same time as
   the accidental wake-up prevention feature.

configpanel/src/com/cyanogenmod/settings/device/ButtonSettingsFragment.java
@@ -17,12 +17,14 @@
 
 package com.cyanogenmod.settings.device;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
@@ -46,10 +48,16 @@ public class ButtonSettingsFragment extends PreferenceFragment
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
         String node = Constants.sBooleanNodePreferenceMap.get(preference.getKey());
         if (!TextUtils.isEmpty(node) && FileUtils.isFileWritable(node)) {
             Boolean value = (Boolean) newValue;
             FileUtils.writeLine(node, value ? "1" : "0");
+            if (Constants.FP_WAKEUP_KEY.equals(preference.getKey())) {
+                value &= prefs.getBoolean(Constants.FP_POCKETMODE_KEY, false);
+                Utils.broadcastCustIntent(getContext(), value);
+            }
             return true;
         }
         node = Constants.sStringNodePreferenceMap.get(preference.getKey());
@@ -57,6 +65,12 @@ public class ButtonSettingsFragment extends PreferenceFragment
             FileUtils.writeLine(node, (String) newValue);
             return true;
         }
+
+        if (Constants.FP_POCKETMODE_KEY.equals(preference.getKey())) {
+            Utils.broadcastCustIntent(getContext(), (Boolean) newValue);
+            return true;
+        }
+
         return false;
     }
 
@@ -87,6 +101,10 @@ public class ButtonSettingsFragment extends PreferenceFragment
                 l.setEnabled(false);
             }
         }
+
+        // Initialize other preferences whose keys are not associated with nodes
+        SwitchPreference b = (SwitchPreference) findPreference(Constants.FP_POCKETMODE_KEY);
+        b.setOnPreferenceChangeListener(this);
     }
 
     private void updatePreferencesBasedOnDependencies() {

configpanel/src/com/cyanogenmod/settings/device/Startup.java
@@ -22,6 +22,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 
 import org.cyanogenmod.internal.util.FileUtils;
@@ -34,6 +36,7 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
         if (cyanogenmod.content.Intent.ACTION_INITIALIZE_CM_HARDWARE.equals(action)) {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
             // Disable button settings if needed
             if (!hasButtonProcs()) {
@@ -56,6 +59,12 @@ public class Startup extends BroadcastReceiver {
                             " failed while restoring saved preference values");
                     }
                 }
+
+                // Send initial broadcasts
+                final boolean shouldEnablePocketMode =
+                        prefs.getBoolean(Constants.FP_WAKEUP_KEY, false) &&
+                        prefs.getBoolean(Constants.FP_POCKETMODE_KEY, false);
+                Utils.broadcastCustIntent(context, shouldEnablePocketMode);
             }
         }
     }

pocketmode/src/com/cyanogenmod/pocketmode/Startup.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,15 +20,20 @@ package com.cyanogenmod.pocketmode;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserHandle;
 import android.util.Log;
 
-public class BootCompletedReceiver extends BroadcastReceiver {
+public class Startup extends BroadcastReceiver {
 
     private static final String TAG = "XiaomiPocketMode";
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
-        Log.d(TAG, "Starting");
-        context.startService(new Intent(context, PocketModeService.class));
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (cyanogenmod.content.Intent.ACTION_INITIALIZE_CM_HARDWARE.equals(action)) {
+            Log.d(TAG, "Starting");
+            context.startServiceAsUser(new Intent(context, PocketModeService.class),
+                    UserHandle.CURRENT);
+        }
     }
 }