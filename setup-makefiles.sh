LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Add natrium to build guard
Change-Id: I5299bc6d320078acd13fff63b6d4b127548d3d04

setup-makefiles.sh
@@ -37,7 +37,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true" "$1"
 
 # Copyright headers and guards
-write_headers "gemini"
+write_headers "gemini natium"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt

semdoc/device_xiaomi_msm8996-common

setup-makefiles.sh
@@ -37,7 +37,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true" "$1"
 
 # Copyright headers and guards
-write_headers "gemini"
+write_headers "gemini natrium"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt